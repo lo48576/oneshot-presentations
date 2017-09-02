75891bbc2043c3cbdd8e6c97f6a19032ebd9745f
YOSHIOKA Takuma <tashioka.256@gmail.com>
2017-09-03 00:10:41 +0900

    Add CC 4.0 related stuff and a license page

--- titech-ssr/rogysemi/2017-09-contribution/build.sh
+++ titech-ssr/rogysemi/2017-09-contribution/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+cd "$(dirname "$0")"
+
 build() {
     SRC="${1:-}"
     if [ -z "$SRC" ] ; then
@@ -19,17 +21,23 @@ successful_exit() {
 
 SUBCMD="${1:-build}"
 SOURCE='slide.tex'
+COMMON_RES_DIR='../../resources'
 
 case "$SUBCMD" in
     build)
+        make -C "${COMMON_RES_DIR}"
         build "$SOURCE"
         ;;
     watch)
+        make -C "${COMMON_RES_DIR}"
         trap 'successful_exit' 1 2 3 15
         while : ; do
             build "$SOURCE" || true
             inotifywait 'slide.tex'
         done
+        ;;
+    onlydeps)
+        make -C "${COMMON_RES_DIR}"
         ;;
     *)
         echo "Unknown subcommand ${SUBCMD}" >&2
